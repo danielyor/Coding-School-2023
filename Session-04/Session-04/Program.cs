fa1a2345032cd8a989a40ab0918661405e80bf92
Daniel Yordanov <22645152+danielyor@users.noreply.github.com>
2023-01-12 16:27:33 +0200

    04: fix some mistakes

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -3,19 +3,19 @@ string name = "Daniel";
 Console.WriteLine("1) Hello " + name + "!");
 
 // 2) Write a C# program to print the sum of two numbers and the division of those two numbers.
-int a = 13;
-int b = 5;
+float a = 13;
+float b = 5;
 Console.WriteLine("2) Your numbers are: " + a + " and " + b);
 Console.WriteLine("Their sum is: " + (a + b));
-Console.WriteLine("Their substraction is: " + (a - b));
+Console.WriteLine("Their division is: " + (a / b));
 
 // 3) Write a C# program to print the result of specified math operations:
 Console.WriteLine("3) Results:");
 Console.WriteLine( -1+(5*6) );
 Console.WriteLine( 38+(5%7) );
-Console.WriteLine( 14+((-3*6)/7) );
-Console.WriteLine( 2+13/6*6+Math.Sqrt(7) );
-Console.WriteLine( (Math.Pow(6, 4)+Math.Pow(5, 7))/(9%4) );
+Console.WriteLine( 14+((-3*6)/7f) );
+Console.WriteLine( 2+13/6f*6+Math.Sqrt(7) );
+Console.WriteLine( (Math.Pow(6, 4)+Math.Pow(5, 7))/(float)(9%4) );
 
 // 4) Write a C# program that assigns an age and a gender and displays something like: "You are female and look younger than 20."
 int age = 25;
@@ -32,7 +32,7 @@ Console.WriteLine("5) Basic operations:");
 Console.WriteLine(seconds + " seconds are " + minutes + " minutes, " + hours + " hours, " + days + " days, or " + years + " years.");
 
 // 6) Rewrite Program #5 using .Net Libraries.
-TimeSpan time = new TimeSpan(0, 0, 9001);
+TimeSpan time = TimeSpan.FromSeconds(9001);
 Console.WriteLine("6) TimeSpan struct operations:");
 Console.WriteLine(time.TotalSeconds + " seconds are " + time.TotalMinutes + " minutes, " + time.TotalHours + " hours, " + time.TotalDays + " days, or " + time.TotalDays/365 + " years.");
 
