63b10292bef8d66cd9225dc91a3eadf062d0d7c7
Daniel Yordanov <22645152+danielyor@users.noreply.github.com>
2023-01-18 17:31:41 +0200

    07: commit changes

--- Session-07/Session-07/ActionResolver.cs
+++ Session-07/Session-07/ActionResolver.cs
@@ -26,28 +26,33 @@ namespace Session07 {
 
             // log start
 
-
             try {
                 switch (request.Action) {
 
                     case ActionEnum.Convert:
-                        bool parseSucceeded = int.TryParse(request.Input, out int bin);
+                        bool parseSucceeded = int.TryParse(request.Input, out int dec);
                         if (parseSucceeded) {
-                            // convert
-                            // log message
+                            // convert to binary
+
+                            // log message. Use someString.PadLeft(8, '0'); in response
                         }
                         else {
                             Console.WriteLine("Convert action failed, bad input.");
+
                             // log error message
                         }
                         break;
 
                     case ActionEnum.Uppercase:
-                        // do stuff
+                        // check if input is string
+
+                        // if there is a space, return the longest word in uppercase
+
+                        // else return string in uppercase
                         break;
 
                     case ActionEnum.Reverse:
-                        // do stuff
+                        Console.WriteLine(Reverse(request.Input));
                         break;
 
                     default:
@@ -66,6 +71,21 @@ namespace Session07 {
             return response;
         }
 
+        public string Reverse(string input) {
+            string[] str = new string[input.Length];
+            int i = 0;
+
+            if ((input == null) || (input.Length <= 1)) {
+                str[i] = input;
+            }
+            else {
+                //Console.Write(str[str.Length - 1]);
+                str[input.Length - 1] = input;
+                Reverse(input.Substring(0, (str.Length - 1)));
+            }
+
+            return string.Join("", str);
+        }
 
     }
 }
--- Session-07/Session-07/Program.cs
+++ Session-07/Session-07/Program.cs
@@ -4,6 +4,10 @@ using Session07;
 Console.WriteLine("Session 07 Exercise");
 
 ActionRequest request = new ActionRequest("80", ActionEnum.Convert);
+//ActionRequest request = new ActionRequest("You're speaking a bunch of gobbledygook!", ActionEnum.Convert);
+//ActionRequest request = new ActionRequest("Hello, I can speak backwards!", ActionEnum.Convert);
+
+
 ActionResponse response = new();
 ActionResolver resolver = new();
 
