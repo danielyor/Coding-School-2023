37e715de2a8504afa385d096230a6bdc7c822684
Daniel Yordanov <22645152+danielyor@users.noreply.github.com>
2023-01-24 14:02:11 +0200

    11: update LoginForm properties to public

--- Session-11/Session-11/LoginForm.cs
+++ Session-11/Session-11/LoginForm.cs
@@ -5,18 +5,18 @@ using LibSerializer;
 namespace Session_11 {
     public partial class LoginForm : Form {
 
-        private List<Manager> _managers = new List<Manager>();
-        private List<Customer> _customers = new List<Customer>();
-        private List<Car> _cars = new List<Car>();
-        private List<ServiceTask> _serviceTasks = new List<ServiceTask>();
-        private List<Engineer> _engineers = new List<Engineer>();
-        private List<Transaction> _transactions = new List<Transaction>();
-        private List<TransactionLine> _transactionLines = new List<TransactionLine>();
+        public List<Manager> managers = new List<Manager>();
+        public List<Customer> customers = new List<Customer>();
+        public List<Car> cars = new List<Car>();
+        public List<ServiceTask> serviceTasks = new List<ServiceTask>();
+        public List<Engineer> engineers = new List<Engineer>();
+        public List<Transaction> transactions = new List<Transaction>();
+        public List<TransactionLine> transactionLines = new List<TransactionLine>();
         public LoginForm() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        public void Form1_Load(object sender, EventArgs e) {
             PopulateCustomers();
             PopulateManagers();
 
@@ -35,27 +35,27 @@ namespace Session_11 {
             customer1.Surname = "Typou";
             customer1.Phone = "6948192491";
             customer1.TIN = "123456789";
-            _customers.Add(customer1);
+            customers.Add(customer1);
 
             Customer customer2 = new Customer();
             customer2.Name = "Nick";
             customer2.Surname = "Pappas";
             customer2.Phone = "69284820582";
             customer2.TIN = "8575896865";
-            _customers.Add(customer2);
+            customers.Add(customer2);
         }
         public void PopulateManagers() {
             Manager manager1 = new Manager();
             manager1.Name = "Jack";
             manager1.Surname = "Daniels";
             manager1.SalaryPerMonth = 2500;
-            _managers.Add(manager1);
+            managers.Add(manager1);
 
             Manager manager2 = new Manager();
             manager2.Name = "Johnie";
             manager2.Surname = "Walker";
             manager2.SalaryPerMonth = 2200;
-            _managers.Add(manager2);
+            managers.Add(manager2);
         }
 
 
@@ -63,12 +63,12 @@ namespace Session_11 {
             ServiceTask serviceTask1 = new ServiceTask();
             serviceTask1.Description = "Brakes Change";
             serviceTask1.Hours = 2.3M;
-            _serviceTasks.Add(serviceTask1);
+            serviceTasks.Add(serviceTask1);
 
             ServiceTask serviceTask2 = new ServiceTask();
             serviceTask1.Description = "Tires Change";
             serviceTask1.Hours = 1.5M;
-            _serviceTasks.Add(serviceTask1);
+            serviceTasks.Add(serviceTask1);
             //serviceTask1.Code = "";
         }
 
@@ -77,27 +77,27 @@ namespace Session_11 {
             car1.Brand = "Ford";
             car1.Model = "Focus";
             car1.CarRegistrationNumber = "AI78839";
-            _cars.Add(car1);
+            cars.Add(car1);
 
             Car car2 = new Car();
             car2.Brand = "Audi";
             car2.Model = "A4";
             car2.CarRegistrationNumber = "PI78542";
-            _cars.Add(car2);
+            cars.Add(car2);
         }
 
         public void PopulateEngineers() {
             Engineer engineer1 = new Engineer();
             engineer1.Surname = "Nikou";
             engineer1.Name = "Nikos";
-            engineer1.ManagerID = _managers[0].ID;
-            _engineers.Add(engineer1);
+            engineer1.ManagerID = managers[0].ID;
+            engineers.Add(engineer1);
 
             Engineer engineer2 = new Engineer();
             engineer1.Surname = "Xariton";
             engineer1.Name = "Giotis";
-            engineer1.ManagerID = _managers[0].ID;
-            _engineers.Add(engineer1);
+            engineer1.ManagerID = managers[0].ID;
+            engineers.Add(engineer1);
 
         }
 
@@ -105,9 +105,9 @@ namespace Session_11 {
         public void Transaction() {
             Transaction transaction1 = new Transaction();
             transaction1.Date = DateTime.Now;
-            transaction1.CustomerID = _customers[0].ID;
-            transaction1.ManagerID = _managers[0].ID;
-            transaction1.CarID = _cars[0].ID;
+            transaction1.CustomerID = customers[0].ID;
+            transaction1.ManagerID = managers[0].ID;
+            transaction1.CarID = cars[0].ID;
             //transaction1.CustomerID = customerID;
             //transaction1.ManagerID = managerID;
             //transaction1.CarID = carID;
@@ -115,20 +115,20 @@ namespace Session_11 {
         //Guid transactionID, Guid engineerID, Guid serviceTaskID, decimal hours
         public void TransactionLine() {
             TransactionLine transactionLine1 = new TransactionLine();
-            transactionLine1.TransactionID = _transactions[0].ID;
-            transactionLine1.Hours = _serviceTasks[0].Hours;
+            transactionLine1.TransactionID = transactions[0].ID;
+            transactionLine1.Hours = serviceTasks[0].Hours;
             transactionLine1.PricePerHour = 45.5M;
-            transactionLine1.EngineerID = _engineers[0].ID;
-            transactionLine1.ServiceTaskID = _serviceTasks[0].ID;
-            _transactionLines.Add(transactionLine1);
+            transactionLine1.EngineerID = engineers[0].ID;
+            transactionLine1.ServiceTaskID = serviceTasks[0].ID;
+            transactionLines.Add(transactionLine1);
 
             TransactionLine transactionLine2 = new TransactionLine();
-            transactionLine1.TransactionID = _transactions[0].ID;
-            transactionLine1.Hours = _serviceTasks[1].Hours;
+            transactionLine1.TransactionID = transactions[0].ID;
+            transactionLine1.Hours = serviceTasks[1].Hours;
             transactionLine1.PricePerHour = 45.5M;
-            transactionLine1.EngineerID = _engineers[0].ID;
-            transactionLine1.ServiceTaskID = _serviceTasks[1].ID;
-            _transactionLines.Add(transactionLine2);
+            transactionLine1.EngineerID = engineers[0].ID;
+            transactionLine1.ServiceTaskID = serviceTasks[1].ID;
+            transactionLines.Add(transactionLine2);
         }
 
 
@@ -142,21 +142,21 @@ namespace Session_11 {
 
         }
 
-        private void LoginForm_Load(object sender, EventArgs e) {
+        public void LoginForm_Load(object sender, EventArgs e) {
 
         }
 
-        private void btnCustomer_Click(object sender, EventArgs e) {
+        public void btnCustomer_Click(object sender, EventArgs e) {
             CustomerForm customerForm = new();
             customerForm.ShowDialog();
         }
 
-        private void btnManager_Click(object sender, EventArgs e) {
+        public void btnManager_Click(object sender, EventArgs e) {
             ManagerForm managerForm = new();
             managerForm.ShowDialog();
         }
 
-        private void btnEngineer_Click(object sender, EventArgs e) {
+        public void btnEngineer_Click(object sender, EventArgs e) {
             EngineerForm engineerForm = new();
             engineerForm.ShowDialog();
         }
