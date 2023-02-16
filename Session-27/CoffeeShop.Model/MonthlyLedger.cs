493b98f9b8d3ee85fdaeefc4bd99254045a9fd4c
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-16 17:12:06 +0200

    ledger

---
+++ Session-27/CoffeeShop.Model/MonthlyLedger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Model
+{
+    public class MonthlyLedger
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal Total { get; set; }
+
+        MonthlyLedger() 
+        {
+            DateTime now = DateTime.Now;
+            Year = now.Year;
+            Month = now.Month;
+            Total = Income - Expenses;
+        }
+
+    }
+}
