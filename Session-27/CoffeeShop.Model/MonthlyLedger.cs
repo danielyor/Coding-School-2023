811856fd9218c09e0afd63ea4af956e2e0a2b7e9
vlasis votsos <vlasisvotsos@gmail.com>
2023-02-16 18:17:44 +0200

    composite key

--- Session-27/CoffeeShop.Model/MonthlyLedger.cs
+++ Session-27/CoffeeShop.Model/MonthlyLedger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace CoffeeShop.Model
 {
     public class MonthlyLedger
     {
+        [Key]
         public int Year { get; set; }
+        [Key]
         public int Month { get; set; }
         public decimal Income { get; set; }
         public decimal Expenses { get; set; }
