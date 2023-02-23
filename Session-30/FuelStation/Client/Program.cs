244731a43bbb91f9a27728d88c600ac51a18da17
Daniel Yordanov <22645152+danielyor@users.noreply.github.com>
2023-02-23 15:13:37 +0200

    30: add login state handling to client

--- Session-30/FuelStation/Client/Program.cs
+++ Session-30/FuelStation/Client/Program.cs
@@ -1,11 +1,13 @@
 using FuelStation.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using FuelStation.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(ls => new LoginState {});
 
 await builder.Build().RunAsync();
---
+++ Session-30/FuelStation/Shared/LoginState.cs
@@ -0,0 +1,20 @@
+﻿using FuelStation.Model.Enums;
+
+namespace FuelStation.Shared {
+    public class LoginState {
+        public EmployeeType? IsLoggedIn { get; set; } = null;
+        public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public event Action? OnChange;
+
+        public void SetLogin(EmployeeType? login, string user) {
+            IsLoggedIn = login;
+            Username = user;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() {
+            OnChange?.Invoke();
+        }
+    }
+}
