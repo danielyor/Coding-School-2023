b3d92c44ff6fca06d427213273bbe4723d80a7b6
Daniel Yordanov <22645152+danielyor@users.noreply.github.com>
2023-02-25 16:57:59 +0200

    30: add employee CRUDs

---
+++ Session-30/FuelStation/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,80 @@
+﻿using FuelStation.Client.Pages;
+using FuelStation.EF.Repositories;
+using FuelStation.Model;
+using FuelStation.Model.Enums;
+using FuelStation.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuelStation.Server.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase {
+        private readonly IEntityRepo<Employee> _employeeRepo;
+
+        public EmployeeController(IEntityRepo<Employee> employeeRepo) {
+            _employeeRepo = employeeRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeDto>> Get() {
+            var employeeList = _employeeRepo.GetAll();
+
+            return employeeList.Select(employee => new EmployeeDto {
+                Id = employee.Id,
+                Name = employee.Name,
+                Surname = employee.Surname,
+                HireDateStart = employee.HireDateStart,
+                HireDateEnd = employee.HireDateEnd,
+                SalaryPerMonth = employee.SalaryPerMonth,
+                Type = employee.Type
+            });
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<EmployeeDto?> GetById(int id) {
+            var result = _employeeRepo.GetById(id);
+
+            return new EmployeeDto {
+                Id = result.Id,
+                Name = result.Name,
+                Surname = result.Surname,
+                HireDateStart = result.HireDateStart,
+                HireDateEnd = result.HireDateEnd,
+                SalaryPerMonth = result.SalaryPerMonth,
+                Type = result.Type
+            };
+
+        }
+
+        [HttpPost]
+        public async Task Post(EmployeeDto employee) {
+            var newEmployee = new Employee(employee.Name, employee.Surname, employee.SalaryPerMonth, employee.HireDateStart, employee.Type);
+
+            _employeeRepo.Create(newEmployee);
+        }
+
+        [HttpPut]
+        public async Task Put(EmployeeDto employee) {
+            var dbEmployee = await Task.Run(() => { return _employeeRepo.GetById(employee.Id); });
+
+
+            if (dbEmployee is null) {
+                return;
+            }
+
+            dbEmployee.Name = employee.Name;
+            dbEmployee.Surname = employee.Surname;
+            dbEmployee.HireDateStart = employee.HireDateStart;
+            dbEmployee.SalaryPerMonth = employee.SalaryPerMonth;
+            dbEmployee.Type = employee.Type;
+            
+            _employeeRepo.Update(employee.Id, dbEmployee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) {
+            _employeeRepo.Delete(id);
+        }
+
+    }
+}
---
+++ Session-30/FuelStation/Shared/EmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿using FuelStation.Model.Enums;
+
+namespace FuelStation.Shared {
+    public class EmployeeDto {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public DateTime HireDateStart { get; set; }
+        public DateTime? HireDateEnd { get; set; }
+        public int SalaryPerMonth { get; set; }
+        public EmployeeType Type { get; set; }
+
+    }
+}
