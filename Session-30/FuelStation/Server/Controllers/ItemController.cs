adbec7c029897d47d7bdc32c9478f1cd6c18065f
Daniel Yordanov <22645152+danielyor@users.noreply.github.com>
2023-02-25 18:00:14 +0200

    30: add item CRUDs

---
+++ Session-30/FuelStation/Server/Controllers/ItemController.cs
@@ -0,0 +1,77 @@
+﻿using FuelStation.Client.Pages;
+using FuelStation.EF.Repositories;
+using FuelStation.Model;
+using FuelStation.Model.Enums;
+using FuelStation.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuelStation.Server.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase {
+        private readonly IEntityRepo<Item> _itemRepo;
+
+        public ItemController(IEntityRepo<Item> itemRepo) {
+            _itemRepo = itemRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ItemDto>> Get() {
+            var itemList = _itemRepo.GetAll();
+
+            return itemList.Select(item => new ItemDto {
+                Id = item.Id,
+                Code = item.Code,
+                Description = item.Description,
+                Type = item.Type,
+                Price = item.Price,
+                Cost = item.Cost
+            });
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ItemDto?> GetById(int id) {
+            var result = _itemRepo.GetById(id);
+
+            return new ItemDto {
+                Id = result.Id,
+                Code = result.Code,
+                Description = result.Description,
+                Type = result.Type,
+                Price = result.Price,
+                Cost = result.Cost
+            };
+
+        }
+
+        [HttpPost]
+        public async Task Post(ItemDto item) {
+            var newEmployee = new Item(item.Code, item.Description, item.Type, item.Price, item.Cost);
+
+            _itemRepo.Create(newEmployee);
+        }
+
+        [HttpPut]
+        public async Task Put(ItemDto item) {
+            var dbItem = await Task.Run(() => { return _itemRepo.GetById(item.Id); });
+
+            if (dbItem is null) {
+                return;
+            }
+
+            dbItem.Code = item.Code;
+            dbItem.Description = item.Description;
+            dbItem.Type = item.Type;
+            dbItem.Price = item.Price;
+            dbItem.Cost = item.Cost;
+
+            _itemRepo.Update(item.Id, dbItem);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) {
+            _itemRepo.Delete(id);
+        }
+
+    }
+}
---
+++ Session-30/FuelStation/Shared/ItemDto.cs
@@ -0,0 +1,13 @@
+﻿using FuelStation.Model.Enums;
+
+namespace FuelStation.Shared {
+    public class ItemDto {
+        public int Id { get; set; }
+        public string? Code { get; set; } = null;
+        public string Description { get; set; } = string.Empty;
+        public ItemType Type { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+
+    }
+}
